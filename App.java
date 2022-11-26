bd8a914d74e77efb9892e18c479c5f0e43316d69
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-11-25 20:36:50 -0700

    Creating a userAccount / Builder Pattern

--- App.java
+++ App.java
@@ -1,6 +1,24 @@
 public class App {
     public static void main(String[] args) {
-        OnlineAccount troyJohnson = new OnlineAccount(1L, "Troy Johnson", "511 McCurdy Rd", 1000L, 4L);
-        OnlineAccount danRomero = new OnlineAccount(2L, "Dan Romero", "423 El Lano Rd", 4000L, 10L);
+        OnlineAccount troyJohnson = new OnlineAccount.Builder(1L)
+        .withName("Troy Johnson")
+        .withAddress("511 McCurdy Rd")
+        .withBudget(1000L)
+        .withDiscount(2L)
+                .build();
+
+
+        OnlineAccount danRomero = new OnlineAccount.Builder(1L)
+        .withName("Dan Romero")
+        .withAddress("323 Calle del Sol")
+        .withBudget(1500L)
+        .withDiscount(4L)
+                .build();
+
+        System.out.println(troyJohnson.getName() + ": " + troyJohnson.getAddress());
+
+
+        System.out.println(danRomero.getName() +  ": " + danRomero.getAddress());
+                
     }
 }
--- OnlineAccount.java
+++ OnlineAccount.java
@@ -4,13 +4,112 @@ public class OnlineAccount {
     private String address;
     private Long budget;
     private Long discountRate;
+
+    //Creating 'Builder' from "Builder" Pattern Diagram
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String address;
+        private Long budget;
+        private Long discountRate;
+
+        //Setters to return the object within the constructor
+
+        public Builder(Long id) {
+            this.id = id; 
+        }
+
+        public Builder withName(String name){
+            this.name = name; 
+
+            return this;
+        }
+
+        public Builder withAddress(String address) {
+            this.address = address;
+            
+            return this;
+        }
+
+        public Builder withBudget(Long budget){
+            this.budget = budget; 
+
+            return this;
+        }
+
+        public Builder withDiscount(Long discountRate){
+            this.discountRate = discountRate; 
+
+            return this; 
+        }
+
+        public OnlineAccount build() {
+            OnlineAccount onlineAccount = new OnlineAccount();
+            onlineAccount.id = this.id;
+            onlineAccount.name = this.name; 
+            onlineAccount.address = this.address;
+            onlineAccount.budget = this.budget; 
+            onlineAccount.discountRate = this.discountRate; 
+
+            return onlineAccount; 
+        }
+
+
     
-    public OnlineAccount(Long id, String name, String address, Long budget, Long discountRate){
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.discountRate = discountRate;
+    }
+
+
+    private OnlineAccount() {
 
     }
+
+    public Long getId() {
+        return id;
+    }
     
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public Long getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Long budget) {
+        this.budget = budget;
+    }
+
+    public Long getDiscount() {
+        return discountRate;
+    }
+
+    public void setDisctount(Long discoutRate) {
+        this.discountRate = discoutRate;
+    }
+    
+    
+    // public OnlineAccount(Long id, String name, String address, Long budget, Long discountRate){
+    //     this.id = id;
+    //     this.name = name;
+    //     this.address = address;
+    //     this.discountRate = discountRate;
+
+    // }
 }
