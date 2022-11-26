ddb84804077fef48330321edec4892391176bfe4
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-11-25 17:22:04 -0700

    First commit OnlineStore

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        OnlineAccount troyJohnson = new OnlineAccount(1L, "Troy Johnson", "511 McCurdy Rd", 1000L, 4L);
+        OnlineAccount danRomero = new OnlineAccount(2L, "Dan Romero", "423 El Lano Rd", 4000L, 10L);
+    }
+}
---
+++ OnlineAccount.java
@@ -0,0 +1,16 @@
+public class OnlineAccount {
+    private Long id;
+    private String name;
+    private String address;
+    private Long budget;
+    private Long discountRate;
+    
+    public OnlineAccount(Long id, String name, String address, Long budget, Long discountRate){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.discountRate = discountRate;
+
+    }
+    
+}
